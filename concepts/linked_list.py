0b3717e297dbe89c3825fd9b442cdcf7b546a6a9
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-08 21:00:36 -0800

    Working on linked list

---
+++ concepts/linked_list.py
@@ -0,0 +1,28 @@
+#Linked List Implementation in python
+from typing import Any
+class Node:
+    def __init__(self, data:Any, next=None, prev=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_at_beginning(self,data):
+        pass
+
+    def insert_at_end(self,data):
+        pass
+
+    def insert_at(self,data):
+        pass
+
+    def remove_at(self,data):
+        pass
+
+    def insert_values(self,values):
+        pass
+
+
