ec7a6c74118517ad68efa0ae7cdf9af76052e2b6
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-11 21:59:23 -0800

    Added solution for 2 sum

--- problems/lc1_two_sum.py
+++ problems/lc1_two_sum.py
@@ -1,1 +1,15 @@
 # implementing 2 sum
+
+class TwoSum:
+    def __init__(self):
+        pass
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        two_dict = {} #create a dictionary to act as a hash map
+        for i in range(len(nums)): 
+            diff = target-nums[i] #use the difference as a key in the hash map
+            if diff in two_dict:
+                return([two_dict[diff],i])                
+            else:
+                two_dict[nums[i]] = i
+        return None
