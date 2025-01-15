2bed16e94e914668a4f809121d4778ee4e51f710
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-15 12:04:32 -0800

    Working on linked list feature in merging 2 sorted lists

--- problems/lc21_merge_two_sorted_lists.py
+++ problems/lc21_merge_two_sorted_lists.py
@@ -1,5 +1,10 @@
 #Solution for merging 2 sorted lists
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class MergeTwoLists:
     def mergeTwoLists(self, list1: list, list2: list) -> list:
         i=0
@@ -17,7 +22,32 @@ class MergeTwoLists:
 
         return listr
 
+    def mergeTwoLinkedLists(self, list1, list2):
+        #2 linked lists are passed in instead. The nodes are mentioned above. 
+        pass
+
+def build_linked_list(elements):
+    head = ListNode(val=elements[0],next=None)
+    pointer=head
+    for elem in elements[1:]:
+        #print(elem)
+        temp_node = ListNode(elem,next=None)
+        pointer.next = temp_node
+        pointer = pointer.next        
+
+    return head
+
+def print_linked_list(head_node):
+    pointer = head_node
+    while pointer:
+        print(f'{pointer.val}-->',end="")
+        pointer=pointer.next
+
+
 if __name__=='__main__':
     merge = MergeTwoLists()
     my_list = merge.mergeTwoLists([1,2,3,10],[1,6,7])
-    print(my_list)
+    #print(my_list)
+
+    my_ll = build_linked_list([34,56,78,99])
+    print_linked_list(my_ll)
