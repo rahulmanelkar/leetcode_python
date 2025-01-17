b22cb51d1aea0571875028d58833587469854ab1
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-16 21:33:25 -0800

    Added solution for lc 58

--- problems/lc58_length_of_last_word.py
+++ problems/lc58_length_of_last_word.py
@@ -1,1 +1,8 @@
 #Length of last word. Using python paradigm and then generic
+class LC58:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
+    
+if __name__ == '__main__':
+    lc = LC58()
+    print(lc.lengthOfLastWord("Hello World"))
