83d4969a20bd8e5183634762e9503096ba0835e6
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-18 15:47:29 -0800

    Solved lc 28

--- problems/lc28_first_substring_occcurrence.py
+++ problems/lc28_first_substring_occcurrence.py
@@ -1,1 +1,25 @@
 #Working on lc28
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack)==1:
+            if needle==haystack:
+                return 0
+            else:
+                return -1
+            
+        if len(needle)==1 or len(needle)==len(haystack):
+             cmp = len(haystack)
+        else:
+             cmp = len(haystack)-len(needle)
+
+        for i in range(cmp+1):
+                if needle == haystack[i:i+len(needle)]:
+                    return i
+        return -1
+    
+if __name__=='__main__':
+    s = Solution()
+    hay = "mississippi"
+    nee = "ppi"
+    print(s.strStr(hay,nee))
