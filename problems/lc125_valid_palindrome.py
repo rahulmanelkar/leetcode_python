45f7e9923886c86df5bd73f8398e09b63dc51272
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-17 20:07:32 -0800

    Added solution for lc 125

--- problems/lc125_valid_palindrome.py
+++ problems/lc125_valid_palindrome.py
@@ -1,1 +1,12 @@
 #valid palindrome given contraints
+
+import re
+def remove_alphanumeric(string):
+    return re.sub(r'[^a-zA-Z0-9]', '', string)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=remove_alphanumeric(s)
+        s=s.lower().replace(" ","")
+        return s==s[::-1]
+        
