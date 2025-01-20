647c24b19501de8546bde41e6d0fc947c1d40bb9
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-19 21:28:56 -0800

    Added suboptimal solution for tc169

--- problems/tc169_majority_element.py
+++ problems/tc169_majority_element.py
@@ -1,2 +1,17 @@
 #Solution for majority element
 
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        highest_count=0
+        highest_index=0
+        for i in range(len(nums)):
+            thecount = nums.count(nums[i])      
+            if thecount>highest_count:
+                highest_count=thecount
+                highest_index=i
+        return nums[highest_index]
+
+
+if __name__=='__main__':
+    s=Solution()
+    print(s.majorityElement([3,2,2,2]))
